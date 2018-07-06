502e5dfa046acdfafceafc33ceeb1ff7cb01183e
Drakirus <pierrescalade@gmail.com>
2018-07-06 22:34:57 +0200

    backup :boom:

---
+++ config/autokey/data/python/to_uppercase.py
@@ -0,0 +1,9 @@
+import time
+try:
+    selText = clipboard.get_selection()
+    time.sleep(0.2)
+    keyboard.send_keys(selText.upper())
+
+except:
+    dialog.info_dialog(title='No text selected', 
+    message='No text in X selection') 
