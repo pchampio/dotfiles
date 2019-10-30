e332d97f7446075e235324d36e824d059da8a755
Drakirus <prr.champion@gmail.com>
2019-10-30 22:33:33 +0100

    alias

--- zsh/g5k.zsh
+++ zsh/g5k.zsh
@@ -4,8 +4,10 @@
 
 # Reserving resources
 # Optimal Allocation of Resources (or Olivier Auguste Richard)
-alias oar-1080="oarsub -p cluster='grele' -q production -l gpu=1/core=1,walltime=24  sleep 24h"
-alias oar-2080="oarsub -p cluster='graffiti' -q production -l gpu=1/core=1,walltime=24  sleep 24h"
+alias oar-1080="oarsub -q production -p \"cluster='grele'\" -l gpu=1/core=1,walltime=24 --stderr=$HOME/.cache/oar/%jobid%-err.log --stdout=$HOME/.cache/oar/%jobid%-out.log 'sleep 24h'"
+alias oar-2080="oarsub -q production -p \"cluster='graffiti'\" -l gpu=1/core=1,walltime=24 --stderr=$HOME/.cache/oar/%jobid%-err.log --stdout=$HOME/.cache/oar/%jobid%-out.log 'sleep 24h'"
+
+# completion zsh
 
 function conda-so-activate(){ source ~/lab/conda/etc/profile.d/conda.sh; conda activate;}
 
