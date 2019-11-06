fedc3e502d48e01419cbc0c89a2326ec40afc4b9
Drakirus <prr.champion@gmail.com>
2019-11-06 20:27:01 +0100

    ba

--- zsh/g5k.zsh
+++ zsh/g5k.zsh
@@ -11,6 +11,10 @@ alias oar-2080="oarsub -q production -p \"cluster='graffiti'\" -l gpu=1/core=1,w
 
 function conda-so-activate(){ source ~/lab/conda/etc/profile.d/conda.sh; conda activate;}
 
+function gg5k(){
+  firefox --new-tab "https://intranet.grid5000.fr/oar/Nancy/drawgantt-svg-prod/drawgantt-svg.php?width=1400&filter=comment%20NOT%20LIKE%20%27Retired%20since%%27%20AND%20gpu%20%3E%200%20AND%20type=%27default%27%20and%20production=%27YES%27&timezone=Europe/Paris&resource_base=host&scale=10&config=prod&scale=20&width=1400&start=$(date +%s  --date='-5 hour')&stop=$(date +%s  --date='+12 hour')"
+}
+
 # wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 # chmod +x Miniconda3-latest-Linux-x86_64.sh
 # bash Miniconda2-latest-Linux-x86_64.sh
